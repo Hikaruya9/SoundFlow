f0547ed7e514d3659119040ab1c138cbf29952dd
gabriel <71945882+Hikaruya9@users.noreply.github.com>
2024-12-04 21:51:56 -0300

    update v0.031

--- src/model/ArtistDAO.java
+++ src/model/ArtistDAO.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Optional;
 
 /**
  *
@@ -19,7 +20,7 @@ import java.util.ArrayList;
 public class ArtistDAO {
     
     public void add(int id, String name, String about, String genre, String imagePath){
-        String sql = "INSERT INTO artist(name,about,genre,image_path) VALUES(?,?,?,?)";
+        String sql = "INSERT INTO artist(name,genre,about,image_path) VALUES(?,?,?,?)";
         try(Connection conn = DB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)){
             int i = 1;
             stmt.setString(i++, name);
@@ -28,13 +29,28 @@ public class ArtistDAO {
             stmt.setString(i++, imagePath);
             stmt.executeUpdate();
         }catch(SQLException e){
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
         }
+    }
+    
+    public Optional<Artist> getById(int id){
+        String sql = "SELECT id,name,genre,about,image_path FROM artist WHERE id = ?";
+        try(Connection conn = DB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                Artist a = new Artist(rs.getInt("id"),rs.getString("name"),rs.getString("genre"),rs.getString("about"),rs.getString("image_path"));
+                return Optional.of(a);
+            }
+        }catch(SQLException e){
+            System.err.println(e.getMessage());
+        }
+        return Optional.empty();
     }
     
     public ArrayList<Artist> getByName(String name){
         ArrayList<Artist> artists = new ArrayList();
-        String sql = "SELECT id,name,about,genre,image_path FROM artist WHERE name LIKE(?)";
+        String sql = "SELECT id,name,genre,about,image_path FROM artist WHERE name LIKE(?)";
         try(Connection conn = DB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)){
             stmt.setString(1, "%"+name+"%");
             ResultSet rs = stmt.executeQuery();
@@ -43,7 +59,7 @@ public class ArtistDAO {
                 artists.add(a);
             }
         }catch(SQLException e){
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
         }
         return artists;
     }
@@ -54,7 +70,7 @@ public class ArtistDAO {
             stmt.setInt(1, id);
             return stmt.executeUpdate() > 0;
         }catch(SQLException e){
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
             return false;
         }
     }
@@ -67,7 +83,7 @@ public class ArtistDAO {
             stmt.setInt(i++, id);
             return stmt.executeUpdate() > 0;
         }catch(SQLException e){
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
             return false;
         }
     }
@@ -80,7 +96,7 @@ public class ArtistDAO {
             stmt.setInt(i++, id);
             return stmt.executeUpdate() > 0;
         }catch(SQLException e){
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
             return false;
         }
     }
@@ -93,7 +109,7 @@ public class ArtistDAO {
             stmt.setInt(i++, id);
             return stmt.executeUpdate() > 0;
         }catch(SQLException e){
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
             return false;
         }
     }
@@ -106,23 +122,23 @@ public class ArtistDAO {
             stmt.setInt(i++, id);
             return stmt.executeUpdate() > 0;
         }catch(SQLException e){
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
             return false;
         }
     }
     
-    public boolean update(int id, String name, String about, String genre, String imagePath){
+    public boolean update(int id, String name, String genre, String about, String imagePath){
         String sql = "UPDATE artist SET name = ?, about = ?, genre = ?, image_path = ? WHERE id = ?";
         try(Connection conn = DB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)){
             int i = 1;
             stmt.setString(i++, name);
-            stmt.setString(i++, about);
             stmt.setString(i++, genre);
+            stmt.setString(i++, about);
             stmt.setString(i++, imagePath);
             stmt.setInt(i++, id);
             return stmt.executeUpdate() > 0;
         }catch(SQLException e){
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
             return false;
         }
     }
--- src/model/ReleaseDAO.java
+++ src/model/ReleaseDAO.java
@@ -5,10 +5,13 @@
 package model;
 
 import connection.DB;
+import data.Release;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Optional;
 
 /**
  *
@@ -16,13 +19,61 @@ import java.sql.ResultSet;
  */
 public class ReleaseDAO {
     
-    public void add(String title) {
+    public void add(String title, String releaseDate, int trackNumber, String length, String imagePath, int artistId) {
         String sql = "INSERT INTO artist_release(title,release_date,track_number,release_length,image_path,artist_id) VALUES(?,?,?,?,?,?)";
         try(Connection conn = DB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)){
             int i = 1;
             stmt.setString(i++, title);
+            stmt.setString(i++, releaseDate);
+            stmt.setInt(i++, trackNumber);
+            stmt.setString(i++, length);
+            stmt.setString(i++, imagePath);
+            stmt.setInt(i++, artistId);
+            stmt.executeUpdate();
         }catch(SQLException e){
-            
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public Optional<Release> getById(int id){
+        String sql = "SELECT id,title,release_date,track_number,release_length,image_path,artist_id FROM artist_release WHERE id = ?";
+        try(Connection conn = DB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                Release r = new Release(rs.getInt("id"), rs.getString("title"), rs.getString("release_date"), rs.getInt("track_number"), rs.getString("release_length"), rs.getString("image_path"), rs.getInt("artist_id"));
+                return Optional.of(r);
+            }
+        }catch(SQLException e){
+            System.err.println(e.getMessage());
+        }
+        return Optional.empty();
+    }
+    
+    public ArrayList<Release> getByTitle(String title){
+        ArrayList<Release> releases = new ArrayList();
+        String sql = "SELECT id,title,release_date,track_number,release_length,image_path,artist_id FROM artist_release WHERE title LIKE(?)";
+        try(Connection conn = DB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, "%"+title+"%");
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                Release r = new Release(rs.getInt("id"), rs.getString("title"), rs.getString("release_date"), rs.getInt("track_number"), rs.getString("release_length"), rs.getString("image_path"), rs.getInt("artist_id"));
+                releases.add(r);
+            }
+        }catch(SQLException e){
+            System.err.println(e.getMessage());
+        }
+        return releases;
+    }
+    
+    public boolean delete(int id){
+        String sql = "DELETE FROM artist_release WHERE id = ?";
+        try(Connection conn = DB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setInt(1, id);
+            return stmt.executeUpdate() > 0;
+        }catch(SQLException e){
+            System.err.println(e.getMessage());
+            return false;
         }
     }
     
